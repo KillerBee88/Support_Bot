6e55aaa123db34eea2f8e8e708acc5cfd0849e50
KillerBee88 <hardmen08@gmail.com>
2024-04-29 15:10:15 +0300

    Update vk_bot.py
    disable bot responses when unable to understand in VK chat

--- vk_bot.py
+++ vk_bot.py
@@ -18,21 +18,24 @@ def detect_intent_texts(project_id, session_id, text, language_code):
 
     response = session_client.detect_intent(
         session=session, query_input=query_input)
-    return response.query_result.fulfillment_text
+    return response
 
 
 def echo(event, vk_api):
-    fulfillment_text = detect_intent_texts(
-        project_id,
-        event.user_id,
-        event.text,
-        'ru',
-    )
-    vk_api.messages.send(
-        user_id=event.user_id,
-        message=fulfillment_text,
-        random_id=random.randint(1, 1000)
-    )
+    project_id = os.getenv("DIALOGFLOW_PROJECT_ID")
+    session_id = event.user_id
+    text = event.text
+    language_code = 'ru'
+
+    response = detect_intent_texts(project_id, session_id, text, language_code)
+
+    if not response.query_result.intent.is_fallback:
+        fulfillment_text = response.query_result.fulfillment_text
+        vk_api.messages.send(
+            user_id=event.user_id,
+            message=fulfillment_text,
+            random_id=random.randint(1, 1000)
+        )
 
 
 if __name__ == "__main__":
