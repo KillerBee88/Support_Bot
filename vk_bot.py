fb991d2814fa3f064cbf743803ec6e26cc189952
KillerBee88 <hardmen08@gmail.com>
2024-04-29 16:54:01 +0300

    Update vk_bot.py
    error handling

--- vk_bot.py
+++ vk_bot.py
@@ -4,7 +4,11 @@ import random
 import vk_api as vk
 from google.cloud import dialogflow
 from vk_api.longpoll import VkEventType, VkLongPoll
+from google.cloud import storage
+import requests
 
+credentials_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+client = storage.Client.from_service_account_json(credentials_path)
 vk_token = os.getenv("VK_BOT_TOKEN")
 project_id = os.getenv("DIALOGFLOW_PROJECT_ID")
 
@@ -38,10 +42,26 @@ def echo(event, vk_api):
         )
 
 
+def send_telegram_message(bot_token, chat_id, message):
+    send_text = 'https://api.telegram.org/bot{}/sendMessage'.format(bot_token)
+    params = {
+        'chat_id': chat_id,
+        'text': message
+    }
+    response = requests.post(send_text, params=params)
+    return response.json()
+
 if __name__ == "__main__":
+    bot_token = os.getenv("TELEGRAM_ERRORBOT_TOKEN")
+    chat_id = os.getenv("TELEGRAM_CHAT_ID")
     vk_session = vk.VkApi(token=vk_token)
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
-    for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api)
+    
+    try:
+        for event in longpoll.listen():
+            if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                echo(event, vk_api)
+    except Exception as e:
+        error_message = "Произошла ошибка: {}".format(str(e))
+        send_telegram_message(bot_token, chat_id, error_message)
