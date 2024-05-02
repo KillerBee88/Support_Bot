17194a927a6916d3b39f9f8bcc9b33e99c1cd65f
KillerBee88 <hardmen08@gmail.com>
2024-05-02 22:43:19 +0300

    Update tg_bot.py

--- tg_bot.py
+++ tg_bot.py
@@ -8,25 +8,15 @@ from telegram import Update
 from telegram.ext import (CallbackContext, CommandHandler, Filters,
                           MessageHandler, Updater)
 
-load_dotenv()
-
 dialogflow_client = dialogflow.SessionsClient()
 project_id = os.getenv("DIALOGFLOW_PROJECT_ID")
-
+developer_chat_id = os.getenv("DEVELOPER_CHAT_ID")
 token = os.getenv("TELEGRAM_BOT_TOKEN")
-
-logging.basicConfig(stream=sys.stdout, level=logging.ERROR,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-error_logger = logging.getLogger('error_logger')
-
-logging.basicConfig(stream=sys.stdout, level=logging.INFO,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-activity_logger = logging.getLogger('activity_logger')
 
 
 def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Напиши привет или задай свой вопрос')
-    activity_logger.info('Start command received')
+    context.bot_data['logger'].info('Start command received')
 
 
 def detect_intent_text(project_id, session_id, text, language_code):
@@ -41,7 +31,7 @@ def detect_intent_text(project_id, session_id, text, language_code):
     return response.query_result.fulfillment_text
 
 
-def echo(update: Update, context: CallbackContext) -> None:
+def handle_message(update: Update, context: CallbackContext) -> None:
     user_id = update.effective_user.id
     text = update.message.text
 
@@ -52,34 +42,44 @@ def echo(update: Update, context: CallbackContext) -> None:
         language_code='ru',
     )
     update.message.reply_text(response_text)
-    activity_logger.info(f'DialogFlow response: {response_text}')
+    context.bot_data['logger'].info(f'DialogFlow response: {response_text}')
 
 
 def error_handler(update: Update, context: CallbackContext) -> None:
-    error_logger.error(
+    context.bot_data['error_logger'].error(
         msg="Exception while handling an update:", exc_info=context.error)
-    
-    developer_chat_id = os.getenv("DEVELOPER_CHAT_ID")
-    
+
     try:
-        context.bot.send_message(chat_id=developer_chat_id, text=str(context.error))
+        context.bot.send_message(
+            chat_id=developer_chat_id, text=str(context.error))
     except Exception as e:
-        error_logger.error(f"Ошибка при отправке сообщения об ошибке: {e}")
+        context.bot_data['error_logger'].error(
+            f"Ошибка при отправке сообщения об ошибке: {e}")
 
 
 def main() -> None:
-    updater = Updater(token, use_context=True)
+    load_dotenv()
 
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO,
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logger = logging.getLogger('activity_logger')
+
+    error_logger = logging.getLogger('error_logger')
+    error_logger.setLevel(logging.ERROR)
+
+    updater = Updater(token, use_context=True)
     dispatcher = updater.dispatcher
+
+    dispatcher.bot_data['logger'] = logger
+    dispatcher.bot_data['error_logger'] = error_logger
 
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(MessageHandler(
-        Filters.text & ~Filters.command, echo))
+        Filters.text & ~Filters.command, handle_message))
 
     dispatcher.add_error_handler(error_handler)
 
     updater.start_polling()
-
     updater.idle()
 
 
