b1a9d4cc88a53165790e1ad74f81368ee92fe3f6
KillerBee88 <hardmen08@gmail.com>
2024-05-02 22:43:21 +0300

    Update vk_bot.py

--- vk_bot.py
+++ vk_bot.py
@@ -4,11 +4,9 @@ import random
 
 import vk_api as vk
 from dotenv import load_dotenv
-from telegram import Bot
 from google.cloud import dialogflow, storage
+from telegram import Bot
 from vk_api.longpoll import VkEventType, VkLongPoll
-
-load_dotenv()
 
 credentials_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
 client = storage.Client.from_service_account_json(credentials_path)
@@ -16,10 +14,6 @@ vk_token = os.getenv("VK_BOT_TOKEN")
 project_id = os.getenv("DIALOGFLOW_PROJECT_ID")
 telegram_bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
 telegram_chat_id = os.getenv("DEVELOPER_CHAT_ID")
-
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
 
 
 def detect_intent_texts(project_id, session_id, text, language_code):
@@ -34,13 +28,10 @@ def detect_intent_texts(project_id, session_id, text, language_code):
     return response
 
 
-def echo(event, vk_api):
+def process_event(event, vk_api, logger):
     session_id = event.user_id
     text = event.text
     language_code = 'ru'
-
-    if text.lower() == 'error':
-        raise Exception("Тестовая ошибка")
 
     response = detect_intent_texts(project_id, session_id, text, language_code)
 
@@ -53,14 +44,20 @@ def echo(event, vk_api):
         )
         logger.info(
             f"Sent message to user {event.user_id}: {fulfillment_text}")
-        
+
 
 def send_telegram_message(text):
     bot = Bot(token=telegram_bot_token)
     bot.send_message(chat_id=telegram_chat_id, text=text)
 
 
-if __name__ == "__main__":
+def main():
+    load_dotenv()
+
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logger = logging.getLogger(__name__)
+
     vk_session = vk.VkApi(token=vk_token)
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
@@ -68,8 +65,13 @@ if __name__ == "__main__":
     try:
         for event in longpoll.listen():
             if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-                echo(event, vk_api)
+                process_event(event, vk_api, logger)
     except Exception as e:
         error_message = f"Возникла ошибка: {str(e)}"
         logger.error(error_message)
-        send_telegram_message(error_message)
+        send_telegram_message(
+            error_message, telegram_bot_token, telegram_chat_id)
+
+
+if __name__ == "__main__":
+    main()
