bdd6b783b8a0c421f7310793397d1089133ac728
KillerBee88 <hardmen08@gmail.com>
2024-05-03 20:30:07 +0300

    Update vk_bot.py

--- vk_bot.py
+++ vk_bot.py
@@ -4,26 +4,25 @@ import random
 
 import vk_api as vk
 from dotenv import load_dotenv
-from google.cloud import storage
+from google.cloud import dialogflow, storage
 from telegram import Bot
 from vk_api.longpoll import VkEventType, VkLongPoll
 
-from dialogflow_helpers import detect_vk_intent_texts
+from dialogflow_helpers import detect_intent_texts
 
-credentials_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-client = storage.Client.from_service_account_json(credentials_path)
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 project_id = os.getenv("DIALOGFLOW_PROJECT_ID")
-telegram_bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
-telegram_chat_id = os.getenv("DEVELOPER_CHAT_ID")
 
 
-def process_event(event, vk_api, logger):
-    session_id = event.user_id
+def process_event(event, vk_api, dialogflow_client, logger):
+    session_id = str(event.user_id)
     text = event.text
     language_code = 'ru'
 
-    response = detect_vk_intent_texts(
-        project_id, session_id, text, language_code)
+    response = detect_intent_texts(
+        project_id, session_id, text, language_code, dialogflow_client)
 
     if not response.query_result.intent.is_fallback:
         fulfillment_text = response.query_result.fulfillment_text
@@ -39,19 +38,21 @@ def process_event(event, vk_api, logger):
 def main():
     load_dotenv()
     vk_token = os.getenv("VK_BOT_TOKEN")
-
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    logger = logging.getLogger(__name__)
+    telegram_bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
+    telegram_chat_id = os.getenv("DEVELOPER_CHAT_ID")
+    credentials_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
 
     vk_session = vk.VkApi(token=vk_token)
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
 
+    dialogflow_client = dialogflow.SessionsClient.from_service_account_json(
+        credentials_path)
+
     try:
         for event in longpoll.listen():
             if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-                process_event(event, vk_api, logger)
+                process_event(event, vk_api, dialogflow_client, logger)
     except Exception as e:
         error_message = f"Возникла ошибка: {str(e)}"
         logger.error(error_message)
